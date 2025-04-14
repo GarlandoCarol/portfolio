6ceca99369673a5e1d713412d0f05463746acf42
GarlandoCarol <garlando.carol@gmail.com>
2025-04-14 10:39:39 +0000

    Correzione js

--- script.js
+++ script.js
@@ -1,63 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sliders = document.querySelectorAll('.slider-wrapper');
 
-    sliders.forEach(slider => {
-        const sliderContainer = slider.querySelector('.slider');
-        const slides = sliderContainer.querySelectorAll('.combo-card');
-        const prevBtn = slider.querySelector('.carousel-btn.left');
-        const nextBtn = slider.querySelector('.carousel-btn.right');
-        const dots = slider.querySelectorAll('.slider-dot');
-
-        let currentIndex = 0;
-
-        function updateSlider(index) {
-            if (index >= slides.length) {
-                currentIndex = 0;  
-            } else if (index < 0) {
-                currentIndex = slides.length - 1;  
-            } else {
-                currentIndex = index;
-            }
-
-            const offset = -currentIndex * 100;
-            sliderContainer.style.transform = `translateX(${offset}%)`;
-
-            dots.forEach(dot => dot.classList.remove('active'));
-            if (dots.length > 0) {
-                dots[currentIndex].classList.add('active');
-            }
-        }
-
-       
-        prevBtn.addEventListener('click', () => {
-            updateSlider(currentIndex - 1);
+    document.addEventListener('DOMContentLoaded', () => {
+        const swipers = document.querySelectorAll('.mySwiper');
+      
+        swipers.forEach((slider) => {
+          new Swiper(slider, {
+            cssMode: true,
+            navigation: {
+              nextEl: slider.querySelector('.swiper-button-next'),
+              prevEl: slider.querySelector('.swiper-button-prev'),
+            },
+            pagination: {
+              el: slider.querySelector('.swiper-pagination'),
+              clickable: true,
+            },
+            mousewheel: true,
+            keyboard: true,
+          });
         });
-
-        nextBtn.addEventListener('click', () => {
-            updateSlider(currentIndex + 1);
-        });
-
-        
-        dots.forEach((dot, index) => {
-            dot.addEventListener('click', () => {
-                updateSlider(index);
-            });
-        });
-
-        updateSlider(currentIndex);
-    });
-
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
+      });
